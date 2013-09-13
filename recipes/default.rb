88144753709da3b5fe6fbb8f748cfedad176e256
Frank Mitchell <me@frankmitchell.org>
2013-09-13 07:33:54 -0400

    This is the code that does the actual email work.

---
+++ recipes/default.rb
@@ -0,0 +1,13 @@
+include_recipe 'chef_handler'
+
+handler_path = node['chef_handler']['handler_path']
+handler = ::File.join handler_path, 'send_email'
+
+cookbook_file "#{handler}.rb" do
+  source 'send_emal.rb'
+end
+
+chef_handler 'BeFrank::SendEmail' do
+  source handler
+  action :enable
+end
